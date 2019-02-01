a3972b91b434c118efbe950ecfac5a2cd6b71c4c
Loic <loic.caron@isen.yncrea.fr>
2019-02-01 23:19:38 +0100

    Test wall.png

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -29,7 +29,10 @@ class MainMenu extends Phaser.Scene {
 
   create ()
   {
-    this.clickButton = this.add.text(400, 300, 'Play!', { fill: '#0f0' })
+    /*var player = prompt("Entrez le nom du directeur :", "Gilbert Montagné");
+    this.username = player;*/
+    this.username = "Gilbert Montagné";
+    this.clickButton = this.add.text(this.cameras.main.width/2, this.cameras.main.height/2 + 100, 'Play!', { fill: '#0f0' })
       .setInteractive()
       .on('pointerdown', () => this.scene.start('loadLevel'));
   }
