f8ec3253ff06bc71a7cb8edcfa21d75055d3e54f
Antoine Hésèque <antoine.heseque@isen.yncrea.fr>
2019-02-01 13:47:47 +0100

    a

--- js/classes/level.js
+++ js/classes/level.js
@@ -15,8 +15,9 @@ class Level {
     var maxHeight = this.levelJSON.layers[0].height;
 
     this.phaser.cameras.main.setBounds(0, 0, maxWidth*32, maxHeight*32);
-    this.phaser.cameras.main.setZoom(0.75);
+    //this.phaser.cameras.main.setViewport(0, 0, maxWidth*32, maxHeight*32);
     //this.cameras.main.centerOn(0, 0);
+    this.phaser.cameras.main.setZoom(0.7);
     for(var y = 0; y < maxHeight; y++){
       for(var x = 0; x < maxWidth; x++){
         // Add dimention array when starting new line
