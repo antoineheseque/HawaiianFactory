68330b3cf360ca88f8da7f96a939c7bfa9aa5388
Antoine Hésèque <antoine.heseque@isen.yncrea.fr>
2019-03-01 15:43:59 +0100

    a

--- start.js
+++ start.js
@@ -4,6 +4,8 @@ var app = express();
 app.use(express.static('assets/'))
 app.use(express.static('js/'))
 app.use(express.static('js/classes/'))
+app.use(express.static('js/stats/'))
+
 app.use(express.static('public/'))
 
 app.get('/', function (req, res) {
