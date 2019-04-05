aa384cf038fa351fbe2520daf36dbcdfc31d7ba0
Antoine Hésèque <hesequeantoine@gmail.com>
2019-04-05 16:21:04 +0200

    Integration tactile

--- js/classes/machine.js
+++ js/classes/machine.js
@@ -11,7 +11,7 @@ class Machine extends Tile{
     this.x = x;
     this.y = y;
     this.createTile();
-    
+
   }
 
   createTile(){
@@ -19,12 +19,14 @@ class Machine extends Tile{
     if(this.stats.upgrades[this.level-1].frames > 1)
       this.image = this.phaser.phaser.add.sprite(this.x*32, this.y*32, this.stats.upgrades[this.level-1].texture + '-1').play(this.stats.upgrades[this.level-1].texture).setOrigin(0, 0).setInteractive().on('pointerdown', () => {
         this.phaser.phaser.UI.getInformations(this);
+        this.phaser.phaser.mouseInteraction.update(this.x,this.y);
       }).on('pointerover', () => {
         this.phaser.phaser.mouseInteraction.update(this.x,this.y);
       });
     else
       this.image = this.phaser.phaser.add.image(this.x*32, this.y*32, this.stats.upgrades[this.level-1].texture).setOrigin(0, 0).setInteractive().on('pointerdown', () => {
         this.phaser.phaser.UI.getInformations(this);
+        this.phaser.phaser.mouseInteraction.update(this.x,this.y);
       }).on('pointerover', () => {
         this.phaser.phaser.mouseInteraction.update(this.x,this.y);
       });
