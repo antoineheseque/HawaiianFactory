c6368375b6977c8793e653ef7b46fddb0279d84e
LouisJ <louis.jeannin@isen.yncrea.fr>
2019-04-08 13:51:42 +0200

    event V1.5

--- js/stats/time.js
+++ js/stats/time.js
@@ -1,11 +1,12 @@
 class Time{
-  constructor(game){
+  constructor(game, level){
     this.game = game;
+    this.level = level;
     this.date = new Date();
 
     this.width = game.cameras.main.width;
     this.height = game.cameras.main.height;
-    this.time = 0
+    this.time = 0;
     this.preload();
   }
 
@@ -30,22 +31,25 @@ class Time{
 
 
   update(time){
-    if(time - this.time > 150){
+    if(time - this.time > 250){
       this.date.setDate(this.date.getDate()+1);
       this.timeText.text = this.formatDate(this.date);
       this.money += this.addMoneyAmount;
+      this.level.actuel = this.time;
       this.time = time;
+      this.level.day = this.date.getDate();
+      this.level.month = this.date.getMonth()+1;
     }
   }
 
   formatDate(date) {
+    //console.log(date);
     var monthNames = [
       "Janvier", "Février", "Mars",
       "Avril", "Mai", "Juin", "Juillet",
       "Août", "Septembre", "Octobre",
       "Novembre", "Décembre"
     ];
-
     var day = date.getDate();
     var monthIndex = date.getMonth();
     var year = date.getFullYear();
