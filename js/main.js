9fd7aed56ce6b27b17419240a7017548faac5ff5
Antoine Hésèque <hesequeantoine@gmail.com>
2019-04-14 06:02:37 +0200

    test1.3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+var width = window.width > 1200 ? 1200 : window.width;
+var height = window.width > 1200 ? 800 : window.height;
+
 const engineConfig = {
     type: Phaser.AUTO,
-    width: window.width, //1200
-    height: window.height, //800
+    width: width, //1200
+    height: height, //800
     pixelArt: false, // true or false
     physics: {
         default: 'arcade',
